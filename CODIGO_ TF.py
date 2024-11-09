591002b6de872ae79355b074b24470db604070fb
Gama <88944546+Code0008@users.noreply.github.com>
2024-11-09 17:22:25 -0500

    Update CODIGO_ TF.py

--- CODIGO_ TF.py
+++ CODIGO_ TF.py
@@ -2,176 +2,195 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from abc import ABC, abstractmethod
 
-
-class Lector_Datos(ABC):
-    def __init__(self):
-        _migracion_internacional = pd.read_excel("caso9_migracion_internacional_clear.xlsx", sheet_name="hoja1", usecols=np.arange(0,21)).dropna().reset_index(drop=True) 
-
-        self._dataframes = {
-            "base_poblacion_proyectada": pd.read_csv("base_poblacion_proyectada2021.csv", sep=";") ,
-            "salidas_segun_continente":  _migracion_internacional.iloc[1:12,:].set_axis(self.columnas(), axis="columns").reset_index(drop=True).drop([0], axis=0).reset_index(drop=True),
-            "entradas_segun_continente":_migracion_internacional.iloc[12:,:].set_axis(self.columnas(), axis="columns").reset_index(drop=True).drop([0], axis=0).reset_index(drop=True) ,
-        }
-
-    def columnas(self, limite_A=2004, limite_B=2024):
-        lista = np.arange(limite_A,limite_B).tolist()
+def columnas():
+        lista = np.arange(2004,2024).tolist()
         lista.insert(0, "Contenido")
         return lista
-    
-    @property
-    def base_poblacional_proyectada(self):
-        return self._dataframes["base_poblacion_proyectada"]
-    @property
-    def salidas_segun_continente(self):
-        return self._dataframes["salidas_segun_continente"]
-    @property
-    def entradas_segun_continente(self):
-        return self._dataframes["entradas_segun_continente"]
-    @property
-    def dataframes(self):
-        return self._dataframes
 
-class Exportador_de_Datos(Lector_Datos):
-    def __init__(self):
-        super().__init__()
-        self.__validadores = Validadores()
-
-    def exportar_salidas(self, *columnas):
-        self.salidas_segun_continente.to_csv("Salidas_mod.csv")
-    
-    def ver_todos_los_dataframes(self):
-        for dataframe in self.dataframes.values():
-            print(dataframe)
-class Reportes_Graficos(Lector_Datos):
-    __fig, __axs = plt.subplots(1,3, figsize=(21,20))
-    __fig.set_size_inches(80,14)
-    def __init__(self):
-        [self.__axs[indice].set_visible(False) for indice in range(0, 3) ]
-        super().__init__()
-
-    def graficador_lineas_tendencias(self, dataframe, ano_A, ano_B, ):
-        # soe = 0 -> salida
-        # soe = 1-> entrada
-        match dataframe:
+def graficador_lineas_tendencias( dataframes, opcion , ano_A, ano_B, ):
+        match opcion:
             case 'S':
-                self.__axs[0].set_title(f"SALIDAS")
-                dataframe = self.salidas_segun_continente
+                plt.title(f"SALIDAS")
+                dataframe = dataframes["salidas_segun_continente"]
             case 'E':
-                self.__axs[0].set_title(f"ENTRADA")
-                dataframe = self.entradas_segun_continente
-        # al ser funciones de unica diferencia los dataframe haremos una unica funcion para graficar ambos reportes (1,2)
+                plt.title(f"ENTRADA")
+                dataframe  =  dataframes["entradas_segun_continente"]
         indices = list(range(dataframe.columns.to_list().index(ano_A), dataframe.columns.to_list().index(ano_B)+1))
-        filtracion_datos = dataframe.iloc[:,indices]
-        valores_continentes = [ filtracion_datos.where(dataframe["Contenido"]==valor).dropna().iloc[0]/100 for valor in dataframe.iloc[np.arange(0,7).tolist(), 0]]
+        filtracion_datos = dataframe.iloc[1:,indices]
+        valores_continentes = [ filtracion_datos.where(dataframe["Contenido"]==valor).dropna().iloc[0]/100 for valor in dataframe.iloc[np.arange(1,9).tolist(), 0]]
         leyenda = []
-        for indice, valor in enumerate(valores_continentes):
-            self.__axs[0].plot(np.arange(ano_A, ano_B+1).tolist(),valor)
+        for indice, valor in enumerate(valores_continentes, start=1):
+            plt.plot(np.arange(ano_A, ano_B+1).tolist(),valor)
             leyenda.append(f"{dataframe.iloc[indice, 0]}")
-        self.__axs[0].legend(leyenda)
-        self.__axs[0].set_ylabel("Milliones de salida")
-        self.__axs[0].set_xlabel("Año")
-        self.__axs[0].set_visible(True)
-    def graficar_barras_continente(self,continente):
+        plt.legend(leyenda)
+        plt.ylabel("Cientos de salida")
+        plt.xlabel("Año")
+        plt.show()
+def graficar_barras_continente(dataframes,continente):
+        plt.title(f"Continente  {continente}")
         datos = [
-            [self.salidas_segun_continente.where(self.salidas_segun_continente["Contenido"]==continente).dropna()[2023],
-            self.entradas_segun_continente.where(self.entradas_segun_continente["Contenido"]==continente).dropna()[2023] ],
-            ["Salidas", 
-            "Entradas" ]
+            [dataframes["salidas_segun_continente"].where(dataframes["salidas_segun_continente"]["Contenido"]==continente).dropna()[2023] /100,       
+             dataframes["entradas_segun_continente"].where(dataframes["entradas_segun_continente"]["Contenido"]==continente).dropna()[2023] /100],
+            ["Salidas", "Entradas" ],
+            ["#C70039", "#DAF7A6"]
         ]
-        for indice in range(len(datos)):
-            self.__axs[2].bar(datos[1][indice], datos[0][indice])
-        self.__axs[2].legend(["Salida", "Entrada"])
-        self.__axs[2].set_visible(True)
-
-    def graficar_barras_continente_diago(self, continente):
-        ano = 2021
-        salidas_continente = self.salidas_segun_continente.where(self.salidas_segun_continente["Contenido"]==continente).dropna()
-        entradas_continente = self.entradas_segun_continente.where(self.entradas_segun_continente["Contenido"]==continente).dropna()
-        #axs[2].bar(x= salidas_continente.columns,  height = salidas_continente.iloc[0:, :] )
-        self.__axs[3].barh("Salidas",  salidas_continente[ano])
-        self.__axs[3].barh("Entradas",  entradas_continente[ano])
-        self.__axs[3].set_visible(True)
-
-
-
-class Reportes_Estadisticos(Lector_Datos):
-    def __init__(self):
-        super().__init__()
-
-class Validadores:
-    def validar_enteros(self, limite_a, limite_b, mensaje):
-        entrada = -1
-
-        while not((limite_a<= entrada <= limite_b ) and type(entrada)==int):
-            entrada = int(input(f"[!] Ingrese {mensaje}"))
-
-        return entrada
-
-    def validar_opciones_multiples(self, *opciones, mensaje, mensaje_opciones):
+        for indice in range(len(datos)-1):
+           plt.bar(datos[1][indice], datos[0][indice], color=datos[2][indice])
+        plt.legend(["Salida", "Entrada"])
+        plt.ylabel("Cientos")
+        plt.show()
         
-        entrada = -1
-        while not (entrada in opciones):
-            for indice, msg in enumerate(mensaje_opciones):
-                print(f"{msg} : {opciones[indice]}")
-            entrada = input(f"Ingrese {mensaje}")
-        return entrada
+
+def graficar_barras_continente_diago( dataframes, continente):
+        plt.title(f"Continente  {continente}")
+
+        #datos = [salidas_segun_continente.where(salidas_segun_continente["Contenido"]=="América").dropna()[2021] , 
+                         # entradas_segun_continente.where(entradas_segun_continente["Contenido"]=="América").dropna()[2021]]
+        datos = [dataframes["salidas_segun_continente"][2021][dataframes["salidas_segun_continente"]["Contenido"]==continente].to_list()[0]/100, 
+                dataframes["entradas_segun_continente"][2021][dataframes["entradas_segun_continente"]["Contenido"]==continente].to_list()[0]/100]
+        #plt.barh("Salidas",  salidas_continente[ano], )
+        #plt.barh("Entradas",  entradas_continente[ano])
+        #plt.barh(np.array(["Salidas","Entradas" ]),  datos)
+        #plt.barh(y=["xd","xdd"], width=datos)
+        plt.barh(y=["Salidas", "Entradas"], width=datos, color=   ["#C70039", "#DAF7A6"])
+        plt.xlabel(f"Cantidad en cientos")
+        plt.show()
+def validar_continente(dataframes):
+        continente =""
+        continentes = dataframes["salidas_segun_continente"]["Contenido"].iloc[1:].values.tolist()
+        while not (continente in list(range( len(continentes)))):
+            for indice, continente_m in enumerate(continentes):
+                print(indice, continente_m)
+            try:
+                continente = int(input("Ingrese opcion continente que quiere ver: ")) 
+            except Exception:
+                pass
+        return continentes[continente]
+def reportes(dataframes):
+    opcion =  -1
+    while not( opcion == 4):
+        print("""
+        1. Lineas de tendencia salida y entrada
+        2. Barra horizontales continente especifico
+        3. Barra diagonales continente especifico
+            """)
+        try:
+            opcion = int(input(f"Seleccione alguna opcion: "))
+        except Exception:
+            pass
+        match opcion:
+            case 1:
+                ano_A= 0
+                dataframe_opcion = ''
+                ano_B= 0
+                while not((ano_A and ano_B in columnas()[1:]) and dataframe_opcion in ['S', 'E']):
+                    try:
+                        dataframe_opcion, ano_A, ano_B = tuple(input("""
+                                                   Seleccione salidas o entradas y los años intervalo en el siguiente formato
+                                                   S: SALIDA
+                                                   E: ENTRADA
+                                                   S|E:20xx:20xx
+                                                   """).split(":"))
+                        dataframe_opcion = dataframe_opcion.upper()
+                        ano_A = int(ano_A)
+                        ano_B = int(ano_B)
+                    except Exception:
+                        pass
+                graficador_lineas_tendencias(dataframes, dataframe_opcion, ano_A=ano_A, ano_B=ano_B)
+            case 2:
+                continente = validar_continente(dataframes)
+                graficar_barras_continente(dataframes,  continente)    
+            case 3:
+                continente = validar_continente(dataframes)
+                graficar_barras_continente_diago(dataframes, continente)
+            case _:
+                  pass
+            
+
+
+def histograma_porcentajes(dataframes):
+    continente = validar_continente(dataframes=dataframes)
+    indice_continente = dataframes["salidas_segun_continente"][ dataframes["salidas_segun_continente"].loc[:, "Contenido"]== continente].index
+    valores = [
+        (
+            (dataframes["salidas_segun_continente"].iloc[indice_continente, 1:] * 100) /
+            dataframes["salidas_segun_continente"].iloc[0, 1:], "Salidas", "skyblue"
+        ),
+        (
+            (dataframes["entradas_segun_continente"].iloc[indice_continente, 1:] * 100) /
+            dataframes["entradas_segun_continente"].iloc[0, 1:], "Entradas", "purple"
+        )
+    ]
+    for tupla in valores:
+        plt.hist(tupla[0].values.flatten(), label=f"{tupla[1]}", color=tupla[2], edgecolor= "black")
     
-class Menu_Principal:
-    __NOMBRE_TRABAJO_FINAL = "INSANINES"
-    def __init__(self) -> None:
-#        self.__reporte_graficos = Reportes_Graficos()
-#        self.__reporte_estadisticos = Reportes_Estadisticos()
-        self.__exportador_de_datos = Exportador_de_Datos()
-        self.__validadores = Validadores()
+    plt.legend()
+    plt.title(f"histograma de porcentaje de Salidas y entradas de {continente}")
+    plt.show()
 
-    def menu_mostrar_datasets(self):
+def sustentacion(dataframes):
+    plt.figure(figsize=(15,9))
 
-        entrada = -1
-        while not (entrada == 3 ):
-                print(f"""
-            Actualmente tenemos {len(self.__exportador_de_datos.dataframes)} datasets
-            con las opcioens de {self.__exportador_de_datos.dataframes.keys()}
-            1. Selecciona dataframe
-            2. Ver todos los dataframes
-            3. Salir pe telen                
-        """)    
-                entrada = self.__validadores.validar_enteros(limite_a=1, limite_b=3, mensaje="seleccione opcion a visualizar: ")
-                match entrada:
-                    case 1:
-                        entrada = self.__validadores.validar_opciones_multiples('BSP',"SSC", "ESC" ,mensaje="Seleccione una opcion: ", mensaje_opciones=self.__exportador_de_datos.dataframes.keys())
-                        match entrada:
-                            case 'BSP':
-                                print(self.__exportador_de_datos.base_poblacional_proyectada)
-                            case 'SSC':
-                                print(self.__exportador_de_datos.salidas_segun_continente)
-                            case 'ESC':
-                                print(self.__exportador_de_datos.salidas_segun_continente)
-                    case 2:
-                        self.__exportador_de_datos.ver_todos_los_dataframes()
+    plt.scatter(dataframes["poblacion_estimada"]["Total"].apply(lambda x: int(x.replace('.',''))) /100 , dataframes["salidas_segun_continente"].iloc[0, 1:] /10 )
+    plt.ylabel("Salidas en decenas", fontdict={"color": "orange",
+                                           "fontsize":22, 
+                                           "fontweight": "bold"})
+    plt.xlabel("estimacion de poblacion en cientos", fontdict={"color": "green",
+                                           "fontsize":22, 
+                                           "fontweight": "bold"})
 
+    plt.title(f"No se puede representaer una buena regresion lineal debido a que falta lineabilidad en los datos :v")
+    plt.show()
+def estadisticas(dataframes):
+    opcion = -1
+    while not(opcion ==3):
+        print(f"""
+            1. Fundamentacion de por que la regresion lineal esa no funciona por que si uwuwuwuwuuuwuwwuwu
+            2. Histograma de porcentajes de salida y entrada de acuerdo a continente :V
+            """)
+        try:
+             opcion = int(input("Ingrese su seleccion: "))
+        except Exception:
+             pass
+        
+        match opcion:
+            case 1:
+                  sustentacion(dataframes=dataframes)
+            case 2:
+                  histograma_porcentajes(dataframes=dataframes)
 
-    def menu_principal(self):
-        opcion = -1
-        while not ( opcion == 5):
-            print(f"""
-        Bienvenido al proyecto {self.__NOMBRE_TRABAJO_FINAL}:
-        opciones:
-            1. Mostrar Datasets disponibles
-            2. Menu Graficos
-            3. Menu Estadisticas
-            4. Menu Exportar datos
-            5. Salir programa 
-                """)
-            opcion =  self.__validadores.validar_enteros(limite_a=0, limite_b=5, mensaje="Ingrese opcion: ")
-
-            match opcion:
-                case 1:
-                    self.menu_mostrar_datasets()
-                case 2:
-                    pass
+def main():
 
 
-men = Menu_Principal()
-men.menu_principal()
+    migracion_internacional = pd.read_excel("caso9_migracion_internacional_clear.xlsx", sheet_name="hoja1", usecols=np.arange(0,21)).dropna().reset_index(drop=True)
+    dataframes = {
+        "salidas_segun_continente": migracion_internacional.iloc[1:10,:].set_axis(columnas(), axis="columns").reset_index(drop=True),
+        "entradas_segun_continente":migracion_internacional.iloc[12:-1,:].set_axis(columnas(), axis="columns").reset_index(drop=True),
+        "poblacion_estimada": pd.read_csv("crecimiento_estimado_inei.csv", sep=";").iloc[np.arange(24,44), :]
+
+    }
+
+
+
+    opcion= -1
+    while not (opcion == 5):
+        print("""
+            1. reportes
+            2. Estadisticas
+            3. Exportar datos
+            """)
+        try:
+            opcion = int(input("Ingrese opcion: "))
+        except Exception:
+            pass
+        match opcion:
+            case 1:
+                reportes(dataframes=dataframes)
+            case 2: 
+                estadisticas(dataframes= dataframes)
+            case _:
+                pass
+
+
+main()
